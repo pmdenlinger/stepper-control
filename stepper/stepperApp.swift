ad0d3ba23782e45a77473521363de2dfe5ff6f17
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 16:54:16 -0700

    Initial Commit

---
+++ stepper/stepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepperApp.swift
+//  stepper
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct stepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
